4e23deff6344d03526a63759bd3450dbd5e4f929
SangHyunJu9803 <xzsdc10@gmail.com>
2025-11-12 20:05:45 +0900

    [FIX] MainMenu의 GameMode 상속 수정
     - GameModeBase -> GamdMode로 수정

--- Source/AO/Public/Game/GameMode/AO_GameMode_MainMenu.h
+++ Source/AO/Public/Game/GameMode/AO_GameMode_MainMenu.h
@@ -3,11 +3,11 @@
 #pragma once
 
 #include "CoreMinimal.h"
-#include "GameFramework/GameModeBase.h"
+#include "GameFramework/GameMode.h"
 #include "AO_GameMode_MainMenu.generated.h"
 
 UCLASS()
-class AO_API AAO_GameMode_MainMenu : public AGameModeBase
+class AO_API AAO_GameMode_MainMenu : public AGameMode
 {
 	GENERATED_BODY()
 
