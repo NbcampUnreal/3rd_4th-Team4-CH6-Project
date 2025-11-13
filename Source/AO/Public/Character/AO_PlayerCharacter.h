9440399a4a27dc76edf0d025f4cb4c13b8a7a984
hongjo77 <76633942+hongjo77@users.noreply.github.com>
2025-11-13 11:03:54 +0900

    [Update]: 플레이어 캐릭터에 ASC, InteractionComponent 추가
    - 플레이어 캐릭터에 ASC, InteractionComponent 추가
    - IMC_Player에 IA_Interact F키로 추가
    - InteractionComponent에서 InteractionWidget 클래스 설정
    - InteractionComponent에서 DefaultAbilities에 GA_Interact, GA_Interact_Trace, GA_Interact_Execute 추가

--- Source/AO/Public/Character/AO_PlayerCharacter.h
+++ Source/AO/Public/Character/AO_PlayerCharacter.h
@@ -4,6 +4,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
 #include "AO_PlayerCharacter.generated.h"
 
 class UCameraComponent;
@@ -11,14 +12,18 @@ class USpringArmComponent;
 class UInputMappingContext;
 class UInputAction;
 struct FInputActionValue;
+class UAbilitySystemComponent;
+class UAO_InteractionComponent;
 
 UCLASS()
-class AO_API AAO_PlayerCharacter : public ACharacter
+class AO_API AAO_PlayerCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
 public:
 	AAO_PlayerCharacter();
+
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
 
 protected:
 	virtual void BeginPlay() override;
@@ -34,6 +39,14 @@ protected:
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PlayerCharacter|Components")
 	TObjectPtr<UCameraComponent> Camera;
+
+	// 승조 : ASC
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerCharacter|Components")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+
+	// 승조 : 상호작용 컴포넌트
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "PlayerCharacter|Components")
+	TObjectPtr<UAO_InteractionComponent> InteractionComponent;
 	
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "PlayerCharacter|Input")
 	TObjectPtr<UInputMappingContext> IMC_Player;
