d616812646e43ad2d5921e48a0627333e2625791
hongjo77 <76633942+hongjo77@users.noreply.github.com>
2025-11-13 21:32:11 +0900

    [Feat] 범용 상호작용 코드 추가
    상호작용 액터 사용법
    C++ 사용법
    1. 이 클래스(AAO_BaseInteractable) 상속 받은 새로운 클래스 생성
    2. 해당 클래스에서 OnInteractionSuccess_BP_Implementation 오버라이드(AO_ExampleInteractable 참고)
    
    블루프린트 사용법
    1. AAO_BaseInteractable 기반으로 블루프린트 생성
    2. Event Graph에서 "Event OnInteractionSuccess_BP" 이벤트 구현

---
+++ Source/AO/Public/Interaction/Base/AO_BaseInteractable.h
@@ -0,0 +1,51 @@
+﻿// HSJ : AO_BaseInteractable.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Interaction/Actor/AO_WorldInteractable.h"
+#include "AO_BaseInteractable.generated.h"
+
+/**
+ * 간단한 상호작용 액터 베이스 클래스
+ * 
+ * 사용법 (C++):
+ * 1. 이 클래스(AAO_BaseInteractable) 상속 받은 새로운 클래스 생성
+ * 2. 해당 클래스에서 OnInteractionSuccess_BP_Implementation 오버라이드(AO_ExampleInteractable 참고)
+ * 
+ * 사용법 (블루프린트):
+ * 1. 이 클래스 기반으로 블루프린트 생성
+ * 2. Event Graph에서 "On Interaction Success BP" 이벤트 구현
+ * 
+ */
+UCLASS(Abstract)
+class AO_API AAO_BaseInteractable : public AAO_WorldInteractable
+{
+	GENERATED_BODY()
+
+public:
+	AAO_BaseInteractable(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+	virtual FAO_InteractionInfo GetInteractionInfo(const FAO_InteractionQuery& InteractionQuery) const override;
+	virtual void GetMeshComponents(TArray<UMeshComponent*>& OutMeshComponents) const override;
+
+	virtual void OnInteractionSuccess(AActor* Interactor) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UStaticMeshComponent> MeshComponent;
+
+	// 상호작용 정보
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Interaction")
+	FText InteractionTitle;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Interaction")
+	FText InteractionContent;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Interaction")
+	float InteractionDuration = 0.0f;
+
+	UFUNCTION(BlueprintNativeEvent, Category="Interaction")
+	void OnInteractionSuccess_BP(AActor* Interactor);
+
+	virtual void OnInteractionSuccess_BP_Implementation(AActor* Interactor);
+};
