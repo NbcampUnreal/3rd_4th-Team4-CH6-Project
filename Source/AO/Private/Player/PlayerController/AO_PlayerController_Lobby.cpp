dee9f6e57a748bdbc178ab81e0c492f494cd6c3f
SangHyunJu9803 <xzsdc10@gmail.com>
2025-11-18 11:04:14 +0900

    [Add]: 로비 레디 로직 구현
     - 기본적인 레디/시작 로직 구현

--- Source/AO/Private/Player/PlayerController/AO_PlayerController_Lobby.cpp
+++ Source/AO/Private/Player/PlayerController/AO_PlayerController_Lobby.cpp
@@ -2,6 +2,7 @@
 
 
 #include "Player/PlayerController/AO_PlayerController_Lobby.h"
+#include "Game/GameMode/AO_GameMode_Lobby.h"
 #include "AO/AO_Log.h"
 
 AAO_PlayerController_Lobby::AAO_PlayerController_Lobby()
@@ -18,4 +19,60 @@ void AAO_PlayerController_Lobby::BeginPlay()
 	bShowMouseCursor = false;
 
 	AO_LOG(LogJSH, Log, TEXT("Lobby PC BeginPlay: InputMode reset to GameOnly (%s)"), *GetName());
+}
+
+void AAO_PlayerController_Lobby::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	if (InputComponent)
+	{
+		// 2키: 레디 토글
+		InputComponent->BindKey(EKeys::Two, IE_Pressed, this, &ThisClass::OnPressed_ReadyKey);
+
+		// 3키: 호스트 시작 요청
+		InputComponent->BindKey(EKeys::Three, IE_Pressed, this, &ThisClass::OnPressed_StartKey);
+	}
+}
+
+void AAO_PlayerController_Lobby::OnPressed_ReadyKey()
+{
+	bIsReady = !bIsReady;
+
+	AO_LOG(LogJSH, Log, TEXT("LobbyPC: Ready key pressed (%s), NewReady=%d"),
+		*GetName(),
+		static_cast<int32>(bIsReady));
+
+	ServerSetReady(bIsReady);
+}
+
+void AAO_PlayerController_Lobby::OnPressed_StartKey()
+{
+	AO_LOG(LogJSH, Log, TEXT("LobbyPC: Start key pressed (%s)"), *GetName());
+
+	ServerRequestStart();
+}
+
+void AAO_PlayerController_Lobby::ServerSetReady_Implementation(bool bNewReady)
+{
+	bIsReady = bNewReady;
+
+	if (UWorld* World = GetWorld())
+	{
+		if (AAO_GameMode_Lobby* GM = World->GetAuthGameMode<AAO_GameMode_Lobby>())
+		{
+			GM->SetPlayerReady(this, bNewReady);
+		}
+	}
+}
+
+void AAO_PlayerController_Lobby::ServerRequestStart_Implementation()
+{
+	if (UWorld* World = GetWorld())
+	{
+		if (AAO_GameMode_Lobby* GM = World->GetAuthGameMode<AAO_GameMode_Lobby>())
+		{
+			GM->RequestStartFrom(this);
+		}
+	}
 }
--- Source/AO/Public/Game/GameMode/AO_GameMode_Lobby.h
+++ Source/AO/Public/Game/GameMode/AO_GameMode_Lobby.h
@@ -16,4 +16,24 @@ class AO_API AAO_GameMode_Lobby : public AGameMode
 
 public:
 	AAO_GameMode_Lobby();
+
+	/** PC가 레디/레디해제 했을 때 호출 (서버) */
+	void SetPlayerReady(AController* Controller, bool bReady);
+
+	/** PC가 시작을 요청했을 때 호출 (서버) */
+	void RequestStartFrom(AController* Controller);
+
+protected:
+	/** 호스트를 제외한 플레이어 중 레디한 컨트롤러 목록 */
+	UPROPERTY()
+	TSet<TObjectPtr<AController>> ReadyPlayers;
+
+	/** 현재 호스트 컨트롤러(PlayerArray[0] 기준) */
+	AController* GetHostController() const;
+
+	/** 호스트를 제외한 모두가 레디인지 확인 */
+	bool IsEveryoneReadyExceptHost() const;
+
+	/** 실제 스테이지로 이동 처리 */
+	void TravelToStage();
 };
