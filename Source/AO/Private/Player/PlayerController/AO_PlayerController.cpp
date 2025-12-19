3131fc88bead78363e36404696075bf22d81a55a
JangJuMan <wnaks9901@gmail.com>
2025-12-19 13:04:03 +0900

    [Fix] : PreClientTravel에서 Delay 삭제
    Delay가 동작하지 않음을 확인, 코드 삭제

--- Source/AO/Private/Player/PlayerController/AO_PlayerController.cpp
+++ Source/AO/Private/Player/PlayerController/AO_PlayerController.cpp
@@ -6,19 +6,20 @@
 #include "AO_Log.h"
 #include "CommonLoadingScreen/Public/LoadingScreenManager.h"
 #include "Online/AO_OnlineSessionSubsystem.h"
-#include "AudioDevice.h"
 #include "Net/VoiceConfig.h"
 #include "VoipListenerSynthComponent.h"
 
 void AAO_PlayerController::PreClientTravel(const FString& PendingURL, ETravelType TravelType, bool bIsSeamlessTravel)
 {
 	AO_LOG(LogJM, Log, TEXT("Start"));
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Yellow, FString::Printf(TEXT("Client Travel To %s"), *PendingURL), false);	// JM : 디버그용
+	
 	if (IsLocalController())
 	{
 		UAO_OnlineSessionSubsystem* OSS = GetGameInstance()->GetSubsystem<UAO_OnlineSessionSubsystem>();
 		if (OSS)
 		{
-			OSS->MuteAllRemoteTalker();	// 뮤트 안해도 되던데? (컴퓨터마다 다른가..?)
+			OSS->MuteAllRemoteTalker();	// JM : 안정성을 위해 Mute도 해주기
 			OSS->StopVoiceChat();
 		}
 		else
@@ -26,30 +27,23 @@ void AAO_PlayerController::PreClientTravel(const FString& PendingURL, ETravelTyp
 			AO_ENSURE(false, TEXT("Can't Get OSS"));
 		}
 
-		/*JM: 이거 지워도 되나? 테스트해보기 : 지우니까 죽는데? */
-		UWorld* CurrentWorld = GetWorld();
-		if (CurrentWorld)
+		UWorld* World = GetWorld();
+		if (World)
 		{
-			// [중요] 2. 실제 에러의 주범인 'VoipListenerSynthComponent'를 모두 찾아 제거
-			// TObjectIterator는 Transient 패키지에 숨은 컴포넌트까지 다 찾아냅니다.
-			// 이거 없어도 되지 않아? 아닌가.. 에디터에서는 안도는데, 패키징에서는 이거빼면 에러뜸
-			for (TObjectIterator<UVoipListenerSynthComponent> It; It; ++It)
+			// JM : 실제 에러의 주범인 'VoipListenerSynthComponent'를 모두 찾아 제거
+			for (TObjectIterator<UVoipListenerSynthComponent> It; It; ++It)		// TObjectIterator는 Transient 패키지에 숨은 컴포넌트까지 다 찾아냄
 			{
-				// 이 컴포넌트가 현재 파괴되려는 월드와 연결되어 있는지 확인
-				if (It->GetWorld() == CurrentWorld)
+				if (It->GetWorld() == World)	// 이 컴포넌트가 현재 파괴되려는 월드와 연결되어 있는지 확인
 				{
-					// It->Stop(); // 재생 중단
-					It->UnregisterComponent(); // 엔진 등록 해제 (이게 핵심!)
-					It->DestroyComponent(); // 파괴
+					It->UnregisterComponent();
+					It->DestroyComponent();
 					AO_LOG(LogJM, Log, TEXT("Cleanup: Forced Unregister SynthComponent"));
 				}
 			}
 
-			// TODO : JM : 이거 없어도 되나?
-			// 3. VOIPTalker 컴포넌트들도 함께 제거
 			for (TObjectIterator<UVOIPTalker> It; It; ++It)
 			{
-				if (It->GetWorld() == CurrentWorld)
+				if (It->GetWorld() == World)
 				{
 					It->Deactivate();
 					It->UnregisterComponent();
@@ -59,39 +53,6 @@ void AAO_PlayerController::PreClientTravel(const FString& PendingURL, ETravelTyp
 			}
 		}
 
-		/*
-		// 2. [테스트용 핵심 코드] 현재 월드의 모든 VOIP Talker 컴포넌트를 찾아서 직접 파괴
-		// Iterator를 사용하면 캐릭터를 일일이 참조하지 않아도 월드 내 모든 토커를 잡을 수 있습니다.
-		if (UWorld* World = GetWorld())
-		{
-			for (TObjectIterator<UVOIPTalker> It; It; ++It)
-			{
-				// 현재 내 월드에 속한 컴포넌트인지 확인
-				if (It->GetWorld() == World)
-				{
-					// 엔진 등록 해제 및 파괴
-					// It->RegisterWithPlayerState(nullptr); // PS와 연결 해제 (크래쉬 유발로 코드 삭제)
-					It->Deactivate();
-					It->UnregisterComponent();
-					It->DestroyComponent();
-                    
-					AO_LOG(LogJM, Log, TEXT("Forced Destruction of VOIPTalker: %s"), *It->GetName());
-				}
-			}
-		}*/
-
-		// 2. 물리적 오디오 장치를 비웁니다. -> 하지마 그냥;;; 소리가 다 꺼지네계속
-		/*if (GEngine)
-		{
-			if (FAudioDeviceHandle AudioDeviceHandle = GEngine->GetMainAudioDevice())
-			{
-				AudioDeviceHandle->StopAllSounds(true);
-				AudioDeviceHandle->Flush(nullptr);
-			}
-		}*/
-
-		GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Yellow, FString::Printf(TEXT("Client Travel To %s"), *PendingURL), false);	// JM : 디버그용
-
 		ULoadingScreenManager* LSM = GetGameInstance()->GetSubsystem<ULoadingScreenManager>();
 		if (LSM)
 		{
@@ -99,24 +60,87 @@ void AAO_PlayerController::PreClientTravel(const FString& PendingURL, ETravelTyp
 		}
 		else
 		{
-			GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Yellow, FString::Printf(TEXT("no lsm")), false);	// JM : 디버그용
 			AO_ENSURE(false, TEXT("Loading Screen Manager is nullptr"));
 		}
 
-		// TODO : 0.5초 딜레이 후에 호출하도록
-		// 1. 정보를 저장
-		PendingTravelInfo = { PendingURL, TravelType, bIsSeamlessTravel };
-
-		// 3. 0.8초 뒤에 진짜 Travel 실행 (컴퓨터 느리면 시간이 더 필요한듯...)
+		/* 해당 딜레이는 동작하지 않음
+		// 0.8초 뒤에 Client Travel 실행 (컴퓨터 느리면 시간이 더 필요한듯...)
+		PendingTravelInfo = { PendingURL, TravelType, bIsSeamlessTravel };	
 		FTimerHandle TravelTimer;
-		GetWorldTimerManager().SetTimer(TravelTimer, this, &AAO_PlayerController::ExecutePreClientTravel, 0.8f, false);
-        
-		return; // 여기서 Super를 호출하지 않고 일단 멈춤
+		GetWorldTimerManager().SetTimer(TravelTimer, this, &AAO_PlayerController::ExecutePreClientTravel, 10.8f, false);
+		return; // 여기서 Super를 호출하지 않고 일단 멈춤*/
 	}
 
 	Super::PreClientTravel(PendingURL, TravelType, bIsSeamlessTravel);
 	AO_LOG(LogJM, Log, TEXT("End"));
 }
+
+/*
+void AAO_PlayerController::ClientTravelInternal_Implementation(const FString& URL, enum ETravelType TravelType,
+	bool bSeamless, FGuid MapPackageGuid)
+{
+	AO_LOG(LogJM, Log, TEXT("Start"));
+
+	// TODO : 함수로 분리하기
+	// if (IsLocalController())		// Client RPC라서 이런 비교가 필요 없음
+	// {
+		UAO_OnlineSessionSubsystem* OSS = GetGameInstance()->GetSubsystem<UAO_OnlineSessionSubsystem>();
+		if (OSS)
+		{
+			OSS->MuteAllRemoteTalker();	// JM : 안정성을 위해 Mute도 해주기
+			OSS->StopVoiceChat();
+		}
+		else
+		{
+			AO_ENSURE(false, TEXT("Can't Get OSS"));
+		}
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			// JM : 실제 에러의 주범인 'VoipListenerSynthComponent'를 모두 찾아 제거
+			for (TObjectIterator<UVoipListenerSynthComponent> It; It; ++It)		// TObjectIterator는 Transient 패키지에 숨은 컴포넌트까지 다 찾아냄
+			{
+				if (It->GetWorld() == World)	// 이 컴포넌트가 현재 파괴되려는 월드와 연결되어 있는지 확인
+				{
+					It->UnregisterComponent();
+					It->DestroyComponent();
+					AO_LOG(LogJM, Log, TEXT("Cleanup: Forced Unregister SynthComponent"));
+				}
+			}
+
+			for (TObjectIterator<UVOIPTalker> It; It; ++It)
+			{
+				if (It->GetWorld() == World)
+				{
+					It->Deactivate();
+					It->UnregisterComponent();
+					It->DestroyComponent();
+					AO_LOG(LogJM, Log, TEXT("Cleanup: Forced Destruction of VOIPTalker"));
+				}
+			}
+		}
+
+		// TODO : 함수로 분리하기
+		ULoadingScreenManager* LSM = GetGameInstance()->GetSubsystem<ULoadingScreenManager>();
+		if (LSM)
+		{
+			LSM->PendingMapName = URL;
+		}
+		else
+		{
+			AO_ENSURE(false, TEXT("Loading Screen Manager is nullptr"));
+		}
+
+		/*
+		FTimerDelegate TravelDelegate;
+		TravelDelegate.BindUObject(this, &AAO_PlayerController::ExecuteClientTravelInternal, URL, TravelType, bSeamless, MapPackageGuid);
+		GetWorldTimerManager().SetTimer(DelayedTravelTimerHandle, TravelDelegate, 10.5f, false);#1#
+	// }
+
+	AO_LOG(LogJM, Log, TEXT("End"));
+	// Super::ClientTravelInternal_Implementation(URL, TravelType, bSeamless, MapPackageGuid);
+}*/
 
 void AAO_PlayerController::CreateSettingsWidgetInstance(const int32 ZOrder, const ESlateVisibility Visibility)
 {
@@ -144,9 +168,19 @@ void AAO_PlayerController::CreateSettingsWidgetInstance(const int32 ZOrder, cons
 	AO_LOG(LogJM, Log, TEXT("End"));
 }
 
-void AAO_PlayerController::ExecutePreClientTravel()
+/*void AAO_PlayerController::ExecutePreClientTravel()
 {
 	AO_LOG(LogJM, Log, TEXT("Start"));
 	Super::PreClientTravel(PendingTravelInfo.URL, PendingTravelInfo.Type, PendingTravelInfo.bSeamless);
 	AO_LOG(LogJM, Log, TEXT("End"));
+}*/
+
+// TODO: 상수 참조 파라미터화
+/*
+void AAO_PlayerController::ExecuteClientTravelInternal(FString URL, ETravelType TravelType, bool bSeamless, FGuid MapPackageGuid)
+{
+	AO_LOG(LogJM, Log, TEXT("Start"));
+	Super::ClientTravelInternal_Implementation(URL, TravelType, bSeamless, MapPackageGuid);
+	AO_LOG(LogJM, Log, TEXT("End"));
 }
+*/
--- Source/AO/Public/Player/PlayerController/AO_PlayerController.h
+++ Source/AO/Public/Player/PlayerController/AO_PlayerController.h
@@ -7,6 +7,8 @@
 #include "UI/Widget/AO_UserWidget.h"
 #include "AO_PlayerController.generated.h"
 
+// JM : ClientTravel 에 딜레이를 주기 위함
+// 딜레이 제대로 동작하지 않아서 삭제 예정
 struct FPendingTravelInfo {
 	FString URL;
 	ETravelType Type;
@@ -24,11 +26,15 @@ class AO_API AAO_PlayerController : public APlayerController
 public:
 	virtual void PreClientTravel(const FString& PendingURL, ETravelType TravelType, bool bIsSeamlessTravel) override;
 
+// protected:
+	// virtual void ClientTravelInternal_Implementation(const FString& URL, enum ETravelType TravelType, bool bSeamless = false, FGuid MapPackageGuid = FGuid()) override;
+
 public:
 	void CreateSettingsWidgetInstance(const int32 ZOrder, const ESlateVisibility Visibility);
 
 private:
-	void ExecutePreClientTravel();
+	// void ExecutePreClientTravel();
+	// void ExecuteClientTravelInternal(FString URL, ETravelType TravelType, bool bSeamless, FGuid MapPackageGuid);
 	
 protected:
 	UPROPERTY(EditDefaultsOnly, Category = "AO|Widget")
@@ -38,5 +44,6 @@ private:
 	UPROPERTY()
 	TObjectPtr<UAO_UserWidget> SettingsWidgetInstance = nullptr;
 
-	FPendingTravelInfo PendingTravelInfo;
+	// FPendingTravelInfo PendingTravelInfo;
+	// FTimerHandle DelayedTravelTimerHandle;
 };
