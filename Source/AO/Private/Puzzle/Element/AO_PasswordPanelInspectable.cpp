889dc4850c129f524cd1c95c1dab7c179a56f9d0
hongjo77 <76633942+hongjo77@users.noreply.github.com>
2025-12-24 10:29:12 +0900

    [Feat]: 패스워드 패널 추가

---
+++ Source/AO/Private/Puzzle/Element/AO_PasswordPanelInspectable.cpp
@@ -0,0 +1,24 @@
+﻿// HSJ : AO_PasswordPanelInspectable.cpp
+#include "Puzzle/Element/AO_PasswordPanelInspectable.h"
+#include "AO_Log.h"
+
+AAO_PasswordPanelInspectable::AAO_PasswordPanelInspectable()
+{
+	InspectionTitle = FText::FromString("Password Panel");
+	InspectionContent = FText::FromString(": Press F to Inspect");
+}
+
+void AAO_PasswordPanelInspectable::OnInspectionMeshClicked(UPrimitiveComponent* ClickedComponent)
+{
+	if (!ClickedComponent || !HasAuthority())
+	{
+		return;
+	}
+	
+	MulticastNotifyButtonClicked(ClickedComponent);
+}
+
+void AAO_PasswordPanelInspectable::MulticastNotifyButtonClicked_Implementation(UPrimitiveComponent* ButtonComponent)
+{
+	OnButtonClickedEvent(ButtonComponent);
+}
---
+++ Source/AO/Public/Puzzle/Element/AO_PasswordPanelInspectable.h
@@ -0,0 +1,24 @@
+﻿// HSJ : AO_PasswordPanelInspectable.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Puzzle/Element/AO_InspectionPuzzle.h"
+#include "AO_PasswordPanelInspectable.generated.h"
+
+UCLASS()
+class AO_API AAO_PasswordPanelInspectable : public AAO_InspectionPuzzle
+{
+	GENERATED_BODY()
+
+public:
+	AAO_PasswordPanelInspectable();
+
+	virtual void OnInspectionMeshClicked(UPrimitiveComponent* ClickedComponent) override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Password")
+	void OnButtonClickedEvent(UPrimitiveComponent* ButtonComponent);
+
+protected:
+	UFUNCTION(NetMulticast, Reliable)
+	void MulticastNotifyButtonClicked(UPrimitiveComponent* ButtonComponent);
+};
