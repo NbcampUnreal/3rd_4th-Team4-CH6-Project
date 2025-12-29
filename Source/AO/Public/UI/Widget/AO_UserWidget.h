e8ce60ef564e7727bb1d5279f7b550aa8480fc54
JangJuMan <wnaks9901@gmail.com>
2025-12-29 16:35:31 +0900

    [HotFix] : UISound 재생 로직 수정
    1. 기존 : 개별 WBP에서 정의해서 사용했으나, 모든 UI가 공통으로 사용해야 하는 기능이기 떄문에 AO_UserWidget에서 정의함
    
    2. 기존 AO_Widget 계열은 해당 함수를 호출하면 됨
    
    [UI Sound 핫픽스!]
    UI 사운드 (호버, 클릭음) 재생 함수 핫픽스 했습니다. (지금 dev에 PR 할거라 나중에 사운드 추가하실 때 Pull 받고 작업해주세요)
    (기존에는 WBP 에서 정의해서 썼는데, 모든 UI에서 사용해야 할 부분이라 AO_UserWidget에 함수 추가했습니다.)
    (사용 방법은 아래와 같습니다.)
    BGM처럼 반복 재생해야 하는 경우엔 Spawn Sound 2D 사용 (관리자 컴포넌트가 붙어야 하기 때문)
    SFX 처럼 잠깐 재생하는 경우엔 Play UISound from Data Table 함수 사용 (사운드는 DT_SoundToPlay에 hover랑 Click 넣어뒀습니다.)
    hover랑 Click 사운드를 나중에 바꾸고 싶을 때 한 번에 바꿀 수 있도록 하기 위함입니다

--- Source/AO/Public/UI/Widget/AO_UserWidget.h
+++ Source/AO/Public/UI/Widget/AO_UserWidget.h
@@ -12,6 +12,26 @@
  * 2. WBP_Base_{category} 를 통해 최상위 부모의 기능을 하위 WBP에서 모두 공유하여 사용
  *  - WBP_Base_Modal : 팝업/모달 류 WBP는 해당 클래스를 상속받아서 일괄적으로 Open/Close 애니메이션이 적용되도록 함
  */
+
+USTRUCT(BlueprintType)
+struct FAO_SoundRow : public FTableRowBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	TObjectPtr<USoundBase> SoundAsset;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float VolumeMultiplier = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float PitchMultiplier = 1.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Description;
+};
+
 UCLASS()
 class AO_API UAO_UserWidget : public UUserWidget
 {
@@ -20,6 +40,10 @@ class AO_API UAO_UserWidget : public UUserWidget
 public:
 	UFUNCTION(BlueprintImplementableEvent, Category = "AO|UI")
 	void OnEscapeCloseRequested();
+
+	// 블루프린트의 PlayUISound를 대체할 함수
+	UFUNCTION(BlueprintCallable, Category = "AO|UI")
+	void PlayUISoundFromDataTable(FName RowName, UDataTable* SoundDataTable);
 	
 protected:
 	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Widget Config")
