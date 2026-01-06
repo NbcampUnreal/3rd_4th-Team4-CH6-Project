e17956895b2aead17a54de420497056da88f1a40
SangHyunJu9803 <xzsdc10@gmail.com>
2026-01-06 12:44:32 +0900

    [Fix]: 나가기 확인 UI 버그 수정
     - 키 입력으로 닫히지 않던 오류 수정

--- Source/AO/Private/UI/Widget/AO_ConfirmReturnToMenuWidget.cpp
+++ Source/AO/Private/UI/Widget/AO_ConfirmReturnToMenuWidget.cpp
@@ -5,6 +5,7 @@
 
 #include "Components/Button.h"
 #include "Components/TextBlock.h"
+#include "UI/AO_UIActionKeySubsystem.h"
 
 void UAO_ConfirmReturnToMenuWidget::NativeOnInitialized()
 {
@@ -24,6 +25,37 @@ void UAO_ConfirmReturnToMenuWidget::NativeOnInitialized()
 	{
 		Txt_Message->SetText(FText::FromString(TEXT("Are you sure you want to return to the main menu?")));
 	}
+}
+
+FReply UAO_ConfirmReturnToMenuWidget::NativeOnPreviewKeyDown(const FGeometry& InGeometry, const FKeyEvent& InKeyEvent)
+{
+	const FKey Key = InKeyEvent.GetKey();
+
+	APlayerController* PC = GetOwningPlayer();
+	if(PC == nullptr)
+	{
+		return Super::NativeOnPreviewKeyDown(InGeometry, InKeyEvent);
+	}
+
+	UGameInstance* GI = PC->GetGameInstance();
+	if(GI == nullptr)
+	{
+		return Super::NativeOnPreviewKeyDown(InGeometry, InKeyEvent);
+	}
+
+	UAO_UIActionKeySubsystem* Keys = GI->GetSubsystem<UAO_UIActionKeySubsystem>();
+	if(Keys == nullptr)
+	{
+		return Super::NativeOnPreviewKeyDown(InGeometry, InKeyEvent);
+	}
+
+	if(Keys->IsUICloseKey(Key))
+	{
+		HandleClicked_Cancel();
+		return FReply::Handled();
+	}
+
+	return Super::NativeOnPreviewKeyDown(InGeometry, InKeyEvent);
 }
 
 void UAO_ConfirmReturnToMenuWidget::HandleClicked_Confirm()
